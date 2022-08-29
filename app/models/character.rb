e430a1cbb5f84bae43cccfb26142055c8dfff89b
Kimani <elvis.kimani@student.moringaschool.com>
2022-08-29 12:34:17 +0300

    commit tree ebf648c66e3d48bc6bee32f40510ba8a7659dd76

--- app/models/character.rb
+++ app/models/character.rb
@@ -1,3 +1,16 @@
 class Character < ActiveRecord::Base
-  
+  belongs_to :show
+  belongs_to :actor
+
+#   def actor
+#     self.actor
+#   end
+
+#   def show
+#     self.show
+#   end
+
+  def say_that_thing_you_say
+     puts "#{self.name} always says: #{self.catchphrase}"
+  end
 end
